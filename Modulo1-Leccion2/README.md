80d55a21ee0a6683f99e0c65aca6e708fb81c053
YarelizCV <yari.catalan11@gmail.com>
2025-04-19 23:56:20 -0400

    modificacion modulo 1 leccion 2

---
+++ Modulo1-Leccion2/README.md
@@ -0,0 +1,11 @@
+# Modulo 1 - Leccion 2: Cliente/Servidor
+
+# GET
+
+![GET](<Screenshot 2025-04-19 233156.png>)
+
+
+
+# POST
+
+![POST](<Screenshot 2025-04-19 233920.png>)
--- Modulo1-Leccion2/app.py
+++ Modulo1-Leccion2/app.py
@@ -2,20 +2,30 @@ from flask import Flask, request , jsonify
 
 app = Flask(__name__)
 
-
+#Ruta GET /info
 @app.route('/info', methods= ['GET'])
 def info():
     return jsonify({
-        "nombre": "Mi primera aplicacion con Flask",
-        "version":"1.0",
-        "descripcion": "Esta es una aplicacion de ejemplo usando Flask creada por Yareliz"
+        "nombre": "Servidor Capstone",
+        "descripcion": "Esta es una aplicacion de API REST"
     })
 
+#Ruta POST /mensaje
 @app.route('/mensaje', methods=['POST'])
-def mensaje():
-    data = request.get_json()
-    respuesta= {"mensaje": f"Bienvenida, {data['nombre']}! Su mensaje fue recibido."}
-    return jsonify(respuesta)
+def mensaje_recibido():
+    print('Metodo recibido:', request.method)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        nombre = data.get('nombre', 'invitado')
+        mensaje = data.get('mensaje', '')
+        return jsonify ({
+            "respuesta": f"Hola, {nombre}. Se ha recibido su mensaje: '{mensaje}' "
+        })
+    else:
+        return jsonify({
+            "mensaje": "Debe utilizar POST para poder enviar un mensaje."
+        })
 
 if __name__ == "__main__":
     app.run(debug=True)
